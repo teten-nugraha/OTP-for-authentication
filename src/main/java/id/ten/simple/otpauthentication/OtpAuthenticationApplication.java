6e7684a8cfbacbba8b8a419f1a32080628c93fc4
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-01-13 10:41:18 +0700

    main

--- src/main/java/id/ten/simple/otpauthentication/OtpAuthenticationApplication.java
+++ src/main/java/id/ten/simple/otpauthentication/OtpAuthenticationApplication.java
@@ -1,9 +1,14 @@
 package id.ten.simple.otpauthentication;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
+@EnableJpaRepositories
 public class OtpAuthenticationApplication {
 
 	public static void main(String[] args) {
