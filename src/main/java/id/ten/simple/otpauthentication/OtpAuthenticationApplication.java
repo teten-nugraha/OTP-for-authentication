48f630198a1556270ba316951bddd82e6fd0b81e
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-01-13 09:25:49 +0700

    first

---
+++ src/main/java/id/ten/simple/otpauthentication/OtpAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package id.ten.simple.otpauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtpAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtpAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ten/simple/otpauthentication/OtpAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ten.simple.otpauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtpAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
